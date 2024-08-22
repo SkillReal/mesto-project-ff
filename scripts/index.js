06086a4d1628b24923b0e0cfafd85f2517c6d51a
Kirill Skvortsov <Kirillskvortsov95@yandex.ru>
2024-08-23 01:30:28 +0300

    Написан JS Код

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,32 @@
 // @todo: Темплейт карточки
+const template = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const container = document.querySelector('.places__list');
+const addButton = document.querySelector('.profile__add-button');
 
 // @todo: Функция создания карточки
+function createCard(info,delCard) {
+ const cardElement = template.querySelector('.card').cloneNode(true);
 
+ cardElement.querySelector('.card__title').textContent = info.name;
+ cardElement.querySelector('.card__image').src = info.link;
+
+ const delButton = cardElement.querySelector('.card__delete-button');
+ delButton.addEventListener('click',delCard);
+ return cardElement;
+}
+  
 // @todo: Функция удаления карточки
-
+function delCard(event) {
+event.target.closest('.card').remove();
+}
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach((cardElement) => {
+  container.append(createCard(cardElement,delCard));
+});
+
+   
+
+
